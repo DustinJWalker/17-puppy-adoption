0d70d45fed8ddf1b2518e520123e51fc15cff601
Dustin Walker <dwtimp@gmail.com>
2016-11-02 14:42:11 -0500

    updating sidebar

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,13 +1,28 @@
 <template lang="html">
   <div class="">
+
     <div class="nav">
       <div class="nav__container">
         <router-link to="/index" class="is-brand">Puppies</router-link>
         <router-link to="/index" class ="is-brand">All Puppies</router-link>
         <router-link to="/new" class="is-brand">Add a Puppy</router-link>
-
       </div>
     </div>
+
+    <div class="sidebar"><p>Adopt a Pupper<p>
+      <router-link class="sidebar__puppies-list"></router-link>
+      <router-link class="sidebar__puppies-image"> </router-link>
+      <router-link class="sidebar__puppies-name"></router-link>
+    </div>
+
+
+
+
+
+
+
+
+
   </div>
 </template>
 
