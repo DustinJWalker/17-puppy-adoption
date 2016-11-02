fe48dde9e967124f5e86c40d3790396535ec531e
Dustin Walker <dwtimp@gmail.com>
2016-11-02 14:31:12 -0500

    set up router links for nav back

---
+++ app/routes/application.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+    <div class="nav">
+      <div class="nav__container">
+        <router-link to="/index" class="is-brand">Puppies</router-link>
+        <router-link to="/index" class ="is-brand">All Puppies</router-link>
+        <router-link to="/new" class="is-brand">Add a Puppy</router-link>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
