303334144240725080707853ecd842b343d3ead8
Dustin Walker <dwtimp@gmail.com>
2016-11-02 19:56:55 -0500

    starting html css with bulma

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,19 +1,49 @@
 <template lang="html">
   <div class="">
 
-    <div class="nav">
-      <div class="nav__container">
-        <router-link to="/index" class="is-brand">Puppies</router-link>
-        <router-link to="/index" class ="is-brand">All Puppies</router-link>
-        <router-link to="/new" class="is-brand">Add a Puppy</router-link>
+    <nav class="nav">
+      <div class="nav__left">
+        <router-link to="/" class="nav-item is-brand">Puppies</router-link>
+      </div>
+      <div class="nav__right">
+        <router-link to="/" class="nav-item is-brand">All Puppies</router-link>
+        <router-link to="/new" class="nav-item is-brand">Add Puppy</router-link>
+      </div>
+    </nav>
+
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-4">
+          <nav class="panel">
+            <p class="panel-heading">
+              Adopt a Pupper
+            </p>
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+
+                </div>
+
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
+  </div>
 
-    <div class="sidebar"><p>Adopt a Pupper<p>
-      <router-link class="sidebar__puppies-list"></router-link>
-      <router-link class="sidebar__puppies-image"> </router-link>
-      <router-link class="sidebar__puppies-name"></router-link>
-    </div>
+
+
+
+
 
 
 
@@ -27,13 +57,24 @@
 </template>
 
 <script>
+const apiUrl =  'https://tiy-tn-class-api-fall-16.herokuapp.com/puppies/ryan';
 export default {
   data() {
     return {
+      apiUrl,
+      puppies: [],
+      path: window.location.pathname,
     };
   },
 
   methods: {
+    getData() {
+      fetch(apiUrl)
+      .then((r) => r.json())
+      .then((puppies) => {
+        this.puppies = puppies;
+      });
+    },
 
   },
 };
