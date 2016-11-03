47986b4db8c43672c1a33885f2bafd367854ff2d
Dustin Walker <dwtimp@gmail.com>
2016-11-03 14:42:42 -0500

    working on tables

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -3,6 +3,7 @@
 
     <nav class="nav">
       <div class="nav__left">
+         <!-- <a class="nav-item is-brand" href="#"> -->
         <router-link to="/" class="nav-item is-brand">Puppies</router-link>
       </div>
       <div class="nav__right">
@@ -12,7 +13,7 @@
     </nav>
 
   <div class="section">
-    <div class="container">
+    <div class="container ">
       <div class="columns">
         <div class="column is-4">
           <nav class="panel">
@@ -29,16 +30,124 @@
 
                 <div class="media-right">
                   <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
 
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
                 </div>
 
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
               </div>
             </div>
           </nav>
         </div>
+
+        <div class="columns is-full width">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th></th>
+              </tr>
+              <tbody>
+                <tr>
+                  <td>Zombie Kid</td>
+                  <td>10</td>
+                  <td>
+                    <a href="">Read More</a>
+                  </td>
+                </tr>
+                <h1>Adopted Puppers</h1>
+                <tr>
+                  <th>Name</th>
+                  <th>Age</th>
+                  <th></th>
+                </tr>
+
+
+              </tbody>
+            </thead>
+
+          </table>
+        </div>
       </div>
     </div>
   </div>
+
+
+
+
 
 
 
