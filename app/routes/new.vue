055e6955f3bc3b55e867ee9f817170ce803e09dc
Dustin Walker <dwtimp@gmail.com>
2016-11-03 15:14:16 -0500

    working on form

---
+++ app/routes/new.vue
@@ -0,0 +1,144 @@
+<template lang="html">
+  <div class="">
+
+    <nav class="nav">
+      <div class="nav__left">
+        <router-link to="/" class="nav-item is-brand">Puppies</router-link>
+      </div>
+      <div class="nav__right">
+        <router-link to="/" class="nav-item is-brand">All Puppies</router-link>
+        <router-link to="/new" class="nav-item is-brand">Add Puppy</router-link>
+      </div>
+    </nav>
+
+  <div class="section">
+    <div class="container ">
+      <div class="columns">
+        <div class="column is-4">
+          <nav class="panel">
+            <p class="panel-heading">
+              Adopt a Pupper
+            </p>
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel-block">
+              <div class="media">
+                <div class="media-left">
+                  <p class="image is-64x64">
+                    <img src="http://placehold.it/128x128" alt="" />
+                  </p>
+                </div>
+
+                <div class="media-right">
+                  <h2>Pupper Name</h2>
+                   <a class="is-active" href="#">Read more</a>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <h1>Found a Pupper?  We can help!</h1>
+      <form action="">
+        <label for="label">Name</label>
+          <p class="control">
+            <input class="input" type="text">
+          </p>
+        <label for="label">Age</label>
+          <p class="control">
+            <input type="input" type="text">
+          </p>
+
+
+      </form>
+    </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
