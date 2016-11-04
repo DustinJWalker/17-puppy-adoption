b1d6565108870cbf2b896a7e10d777935011df57
Dustin Walker <dwtimp@gmail.com>
2016-11-04 09:51:38 -0500

    added formValues to new vue and linked index.js

--- app/routes/new.vue
+++ app/routes/new.vue
@@ -2,12 +2,12 @@
   <div class="">
 
     <nav class="nav">
-      <div class="nav__left">
-        <router-link to="/" class="nav-item is-brand">Puppies</router-link>
+      <div class="nav-left">
+        <router-link to="{ name: 'index' }" class="nav-item is-brand">Puppies</router-link>
       </div>
-      <div class="nav__right">
-        <router-link to="/" class="nav-item is-brand">All Puppies</router-link>
-        <router-link to="/new" class="nav-item is-brand">Add Puppy</router-link>
+      <div class="nav-right">
+        <router-link to="{ name: 'index' }" class="nav-item is-brand">All Puppies</router-link>
+        <router-link to="{ name: 'new' }" class="nav-item is-brand">Add Puppy</router-link>
       </div>
     </nav>
 
@@ -34,80 +34,6 @@
               </div>
             </div>
 
-            <div class="panel-block">
-              <div class="media">
-                <div class="media-left">
-                  <p class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="" />
-                  </p>
-                </div>
-
-                <div class="media-right">
-                  <h2>Pupper Name</h2>
-                   <a class="is-active" href="#">Read more</a>
-                </div>
-              </div>
-            </div>
-
-            <div class="panel-block">
-              <div class="media">
-                <div class="media-left">
-                  <p class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="" />
-                  </p>
-                </div>
-
-                <div class="media-right">
-                  <h2>Pupper Name</h2>
-                   <a class="is-active" href="#">Read more</a>
-                </div>
-              </div>
-            </div>
-
-            <div class="panel-block">
-              <div class="media">
-                <div class="media-left">
-                  <p class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="" />
-                  </p>
-                </div>
-
-                <div class="media-right">
-                  <h2>Pupper Name</h2>
-                   <a class="is-active" href="#">Read more</a>
-                </div>
-              </div>
-            </div>
-
-            <div class="panel-block">
-              <div class="media">
-                <div class="media-left">
-                  <p class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="" />
-                  </p>
-                </div>
-
-                <div class="media-right">
-                  <h2>Pupper Name</h2>
-                   <a class="is-active" href="#">Read more</a>
-                </div>
-              </div>
-            </div>
-
-            <div class="panel-block">
-              <div class="media">
-                <div class="media-left">
-                  <p class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="" />
-                  </p>
-                </div>
-
-                <div class="media-right">
-                  <h2>Pupper Name</h2>
-                   <a class="is-active" href="#">Read more</a>
-                </div>
-              </div>
-            </div>
           </nav>
         </div>
       </div>
@@ -159,9 +85,19 @@
 </template>
 
 <script>
+const apiUrl =  'https://tiy-tn-class-api-fall-16.herokuapp.com/puppies/ryan';
 export default {
   data() {
     return {
+      formValues: {
+        name: '',
+        age: '',
+        sex: '',
+        color: '',
+        breed: '',
+        image_url: '',
+        description: '',
+      }
     };
   },
 
