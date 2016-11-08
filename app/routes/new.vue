a137245df6259c5866e03e34657bc7823c6b6b2f
Dustin Walker <dwtimp@gmail.com>
2016-11-07 21:06:41 -0600

    missing some v-model tags to new vue

--- app/routes/new.vue
+++ app/routes/new.vue
@@ -2,36 +2,37 @@
   <div class="">
     <h1 class="title">Found a Pupper?  We can help!</h1>
     <form @submit.prevent="saveForm">
-      <label for="puppy-name">Name</label>
+      <label for="puppy-name"><strong>Name</strong</label>
       <p class="control">
         <input id="puppy-name" class="input" type="text" v-model="formValues.name">
       </p>
-      <label for="puppy-age">Age</label>
+      <label for="puppy-age"><strong>Age</strong></label>
       <p class="control">
         <input id="puppy-age" class="input" type="text" v-model="formValues.age">
       </p>
-      <label for="puppy-sex">Sex</label>
+      <label for="puppy-sex"><strong>Sex</strong></label>
       <p class="control">
         <span class="select">
             <select v-model="formValues.sex">
+              <option value="">Select Dropdown</option>
               <option>Male</option>
               <option>Female</option>
             </select>
           </span>
       </p>
-      <label for="puppy-color">Color</label>
+      <label for="puppy-color"><strong>Color</strong></label>
       <p class="control">
-        <input id="puppy-color" class="input" type="text">
+        <input id="puppy-color" class="input" type="text" v-model="formValues.color">
       </p>
-      <label for="puppy-breed">Breed</label>
+      <label for="puppy-breed"><strong>Breed</strong></label>
       <p class="control">
-        <input id="puppy-breed" class="input" type="text">
+        <input id="puppy-breed" class="input" type="text" v-model="formValues.breed">
       </p>
-      <label for="puppy-image">Image Url</label>
+      <label for="puppy-image"><strong>Image Url</strong></label>
       <p class="control">
-        <input id="puppy-image" class="input" type="text">
+        <input id="puppy-image" class="input" type="text" v-model="formValues.image_url">
       </p>
-      <label class="puppy-description">Description</label>
+      <label class="puppy-description"><strong>Description</strong></label>
       <p class="control">
         <textarea v-model="formValues.description" class="textarea" placeholder="Textarea"></textarea>
       </p>
